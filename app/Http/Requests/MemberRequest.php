e6a7b7dd453f99805ddc9bee081881f540920083
SAKATA-Atsuki <hhnkkr@icloud.com>
2020-11-17 20:38:48 +0900

    会員登録機能を改良。

--- app/Http/Requests/MemberRequest.php
+++ app/Http/Requests/MemberRequest.php
@@ -14,7 +14,7 @@ class MemberRequest extends FormRequest
      */
     public function authorize()
     {
-        if ($this->path() == route('join'))
+        if ($this->path() == 'join')
         {
             return true;
         } else {
