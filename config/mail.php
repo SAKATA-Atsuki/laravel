7f57a022c61dc906d541fe8f8c3a9b9f21c26006
SAKATA-Atsuki <hhnkkr@icloud.com>
2020-11-24 15:51:09 +0900

    .envのMAILを編集。

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'bc1f17575c-2f2e22@inbox.mailtrap.io'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
