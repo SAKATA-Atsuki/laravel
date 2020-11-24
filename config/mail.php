f7947d8893be1ad00ce677b8ef1f4daa88ff61fc
SAKATA-Atsuki <hhnkkr@icloud.com>
2020-11-24 15:44:47 +0900

    .envのMAILを編集。

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 2525),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('d6ead2dee63324'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('ebaecfde9b0594'),
 
     /*
     |--------------------------------------------------------------------------
