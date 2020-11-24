eb994c226ea8ba1d130524e449eeb01ac8e58034
SAKATA-Atsuki <hhnkkr@icloud.com>
2020-11-24 15:56:28 +0900

    .envのMAILを編集。

--- config/mail.php
+++ config/mail.php
@@ -57,7 +57,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'bc1f17575c-2f2e22@inbox.mailtrap.io'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'name' => env('MAIL_FROM_NAME', 'laravelbbs'),
     ],
 
     /*
