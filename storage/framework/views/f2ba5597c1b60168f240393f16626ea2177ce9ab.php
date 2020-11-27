4d99a7f20d3107b3bac99617d5308bd746b01a58
SAKATA-Atsuki <hhnkkr@icloud.com>
2020-11-27 14:14:34 +0900

    Changed permissions.

---
+++ storage/framework/views/f2ba5597c1b60168f240393f16626ea2177ce9ab.php
@@ -0,0 +1,3 @@
+<p>パスワード再発行</p>
+<p>以下のURLをクリックしてパスワードを再発行してください。</p>
+<a href="<?php echo e($reset_url); ?>"><?php echo e($reset_url); ?></a><?php /**PATH /var/www/laravel/laravelbbs/resources/views/auth/passwords/mail.blade.php ENDPATH**/ ?>
