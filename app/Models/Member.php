197b04adfb8649c050e2811662db9e660da3b126
SAKATA-Atsuki <hhnkkr@icloud.com>
2020-11-27 13:30:24 +0900

    パスワード再設定を実装中。

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\PasswordResetNotification;
 
 class Member extends Authenticatable
 {
@@ -12,4 +13,9 @@ class Member extends Authenticatable
     use Notifiable;
 
     protected $guarded = array('id');
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new PasswordResetNotification($token));
+    }
 }
