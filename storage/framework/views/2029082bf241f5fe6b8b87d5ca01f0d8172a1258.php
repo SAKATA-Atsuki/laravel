5f6015fe9f3da159351ebde53e820239005101a5
SAKATA-Atsuki <hhnkkr@icloud.com>
2020-11-27 13:43:28 +0900

    パスワード再設定を実装中。

--- storage/framework/views/2029082bf241f5fe6b8b87d5ca01f0d8172a1258.php
+++ storage/framework/views/2029082bf241f5fe6b8b87d5ca01f0d8172a1258.php
@@ -1,9 +1,3 @@
-<h1>
-パスワードリセット
-</h1>
-<p>
-以下のボタンを押下し、パスワードリセットの手続きを行ってください。
-</p>
-<p id="button">
-<a href="<?php echo e($reset_url); ?>">パスワードリセット</a>
-</p><?php /**PATH /Applications/MAMP/htdocs/laravelbbs/resources/views/auth/passwords/mail.blade.php ENDPATH**/ ?>
+<p>パスワード再発行</p>
+<p>以下のURLをクリックしてパスワードを再発行してください。</p>
+<a href="<?php echo e($reset_url); ?>"><?php echo e($reset_url); ?></a><?php /**PATH /Applications/MAMP/htdocs/laravelbbs/resources/views/auth/passwords/mail.blade.php ENDPATH**/ ?>
